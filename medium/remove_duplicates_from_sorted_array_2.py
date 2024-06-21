19f256f212eddc45a4ab48c6d86cf444434cd956
Adrian Grosch <adrian.grosch@singular.inf.br>
2024-06-20 23:12:05 -0300

    medium: remake do remove duplicates 2

--- medium/remove_duplicates_from_sorted_array_2.py
+++ medium/remove_duplicates_from_sorted_array_2.py
@@ -3,18 +3,26 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        for index, value in enumerate(nums):
-            counter = 1
-            for _index,_value in enumerate(nums):
-                if index==_index:
-                    continue
-                if value!=_value:
-                    continue
+        counter = 1
+        valid_numbers = 1
+        insert_index = 1
+        for i in range(1, len(nums)):
+            previous = nums[i-1]
+            current = nums[i]
+            if previous == current:
                 counter+=1
-                if counter >2:
-                    nums.remove(_value)
-                
-        return len(nums)
-# nums = [1,1,1,2,2,3]
-nums = [0,0,1,1,1,1,2,3,3]
-print(Solution().removeDuplicates(nums),nums)
+            else:
+                counter=1
+            if counter <=2:
+                nums[insert_index]=current
+                insert_index+=1
+                valid_numbers+=1
+        return valid_numbers
+        
+
+        
+nums=[1,1,1,2,2,3]
+Solution().removeDuplicates(nums)
+print(nums)
+
+
